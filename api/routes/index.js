57a74cf24625cd79d5ea8c53afc2f92b4051f770
Joseph Busse <31547084+joebuss@users.noreply.github.com>
2020-09-24 18:07:00 -0400

    Added in basic test stuff

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/test', function(req, res, next) {
+  res.send('success');
+});
 module.exports = router;
