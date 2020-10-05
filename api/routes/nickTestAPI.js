609232b9c2cbd178d5502e61d1862c754085278e
n1ck-kk <nick@leuer.com>
2020-10-05 14:09:36 -0400

    nick - Added official_school DB and query to get them

--- api/routes/nickTestAPI.js
+++ api/routes/nickTestAPI.js
@@ -13,138 +13,14 @@ let dbInfo = {
     port: 3306,
     multipleStatements: true
   };
+/* --- TEST: correct input, one of missing required things/params --- */
 
-
-router.get('/nickTest', function(req, res, next){
-   // res.send("Nicks second api is working!!!!!!!!!!!!!!");
-    let con = mysql.createConnection(dbInfo);
-
-   
-    con.query("select * from student", (err, results, fields) => {
-        if (err) {
-            console.log(err.stack);
-            con.end();
-            return;
-        }
-        console.log(results);
-        con.end();
-        res.send(results);
-    });
-  
-
-});
-
-router.post('/createStudent', function(req, res, next) {
-    let con = mysql.createConnection(dbInfo);
-    
-    var username = req.body.username;
-    var password = req.body.password;
-    var name = req.body.name;
-    var email = req.body.email;
-    var phone_number = req.body.phone_number || '';
-
-    con.query("insert into student (username, password, name, email, phone_number ) values ( '"+username+"', '"+password+"', '"+name+"', '"+email+"', '"+phone_number+"' )", (err, results, fields) => {
-        if (err) {
-            console.log(err.stack);
-            con.end();
-            return;
-        }
-        console.log(results);
-        con.end();
-        res.send(results);
-    })
-
-})
-
-router.post('/createStudent', function(req, res, next) {
-    let con = mysql.createConnection(dbInfo);
-    
-    var username = req.body.username;
-    var password = req.body.password;
-    var name = req.body.name;
-    var email = req.body.email;
-    var phone_number = req.body.phone_number || '';
-
-    con.query("insert into student (username, password, name, email, phone_number ) values ( '"+username+"', '"+password+"', '"+name+"', '"+email+"', '"+phone_number+"' )", (err, results, fields) => {
-        if (err) {
-            console.log(err.stack);
-            con.end();
-            return;
-        }
-        console.log(results);
-        con.end();
-
-        //call createUserStudent function using results[3]
-        res.send(results);
-    })
-
-})
-
-router.post('/createUser/Student', function(req, res, next) {
-    let con = mysql.createConnection(dbInfo);
-
-    var type_id = req.body.type_id;
-
-    con.query("insert into user (type, type_id) values (1, "+type_id+")", (err, results, fields) => {
-        if (err) {
-            console.log(err.stack);
-            con.end();
-            return;
-        }
-        console.log(results);
-        con.end();
-        res.send(results);
-    })
-})
-
-router.post('/createTeacher', function(req, res, next) {
-    let con = mysql.createConnection(dbInfo);
-    
-    var username = req.body.username;
-    var password = req.body.password;
-    var name = req.body.name;
-    var title = req.body.title;
-    var email = req.body.email;
-    var phone_number = req.body.phone_number || '';
-
-    con.query("insert into teacher (username, password, name, title, email, phone_number ) values ( '"+username+"', '"+password+"', '"+name+"', '"+title+"', '"+email+"', '"+phone_number+"' )", (err, results, fields) => {
-        if (err) {
-            console.log(err.stack);
-            con.end();
-            return;
-        }
-        console.log(results);
-        con.end();
-
-        //call createUserStudent function using results[3]
-        res.send(results);
-    })
-
-})
-
-router.post('/createUser/Teacher', function(req, res, next) {
-    let con = mysql.createConnection(dbInfo);
-
-    var type_id = req.body.type_id;
-
-    con.query("insert into user (type, type_id) values (0, "+type_id+")", (err, results, fields) => {
-        if (err) {
-            console.log(err.stack);
-            con.end();
-            return;
-        }
-        console.log(results);
-        con.end();
-        res.send(results);
-    })
-})
-
+/* --- Function allows teacher to create a class --- */
 router.post('/addClass', function(req, res, next) {
     let name = req.body.name;
     let description = req.body.description;
     let instructor_id = req.body.instructor_id;
     //let student_list = req.body.student_list;
-    
     
     req.checkBody('name', 'Name field is required.').notEmpty();
     req.checkBody('description', 'Description field is required.').notEmpty();
@@ -168,6 +44,7 @@ router.post('/addClass', function(req, res, next) {
 
 });
 
+/* --- Gets list of classes (maybe for recommendation for teacher when creating classes?)--- */
 router.get('/classes', function(req, res, next) {
     let con = mysql.createConnection(dbInfo);
     con.query('select name, description from class', (error, results, fields) => {
@@ -200,6 +77,24 @@ router.get('/instructorSchools', function(req, res, next) {
     });
 });
 
+/* --- official school list --- */
+router.get('/officialSchoolList', function(req, res, next) {
+    let con = mysql.createConnection(dbInfo);
+    con.query('select LocationName from official_schools', (error, results, fields) => {
+        if (error) {
+            console.log(error.stack);
+            con.end();
+            res.status(400).send(error);
+            return;
+        }
+        console.log(results);
+        con.end();
+        res.send(results);
+    });
+});
+
+/* --- Adds a student to a class --- */
+/* --- Updates class's student list and user's class list --- */
 router.post('/addStudentToClass', function(req, res, next) {
     let studentId = req.body.studentId;
     let classId = req.body.classId;
@@ -282,9 +177,6 @@ function addClassToStudent(studentId, classId, req, res, next) {
                 console.log(results2);
                 console.log(`${req.body.classId} successfully added.`);
                 con.end();
-                //req.flash('success', 'Successfully added class to student\'s list.');
-                //res.send(results2);
-                //return results2;
         });
 
     });
