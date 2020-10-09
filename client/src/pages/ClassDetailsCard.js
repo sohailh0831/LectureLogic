64e253ab94ec416a52ac882f25f64820e79b6d81
Paul Miller <mill2136@purdue.edu>
2020-10-09 16:05:47 -0400

    student list button works

--- client/src/pages/ClassDetailsCard.js
+++ client/src/pages/ClassDetailsCard.js
@@ -25,6 +25,8 @@ export default class ClassDetailsCard extends React.Component{
     }
 
     render() {
+        console.log(this.props.studentEmail);
+        console.log(this.props.studentName);
 
         return(
             <div>
